7e8652d2da50b9c6656675e4fbf2eb490a113b16
Pedro Lourenço <pedro.moto4@gmail.com>
2025-10-24 12:54:04 +0100

    debug: Log ALL messages to see what fields are coming

--- src/components/pages/ChatWindow.tsx
+++ src/components/pages/ChatWindow.tsx
@@ -184,15 +184,16 @@ export default function ChatWindow({
               const content = message.content || `[Template: ${message.template_name}]`
               const isMedia = message.kind === 'media'
 
-              // Debug log
-              if (message.media_url || message.media_type) {
-                console.log('Media message detected:', {
-                  kind: message.kind,
-                  media_type: message.media_type,
-                  media_url: message.media_url,
-                  content: message.content
-                })
-              }
+              // Debug log - LOG ALL MESSAGES to see what's coming
+              console.log('Message:', {
+                id: message.id,
+                kind: message.kind,
+                direction: message.direction,
+                content: message.content,
+                media_type: message.media_type,
+                media_url: message.media_url,
+                media_filename: message.media_filename
+              })
 
               return (
                 <div
